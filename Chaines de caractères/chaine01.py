d973eb712c26914dda246bdf918a66459393f345
Marc Augier <m.augier@me.com>
2021-09-01 16:37:12 +0200

    chaines de caractères

---
+++ Chaines de caractères/chaine01.py
@@ -0,0 +1,14 @@
+nom = "andré"
+
+print(nom)
+nom = nom.capitalize()
+
+print(nom)
+
+titre = "le roman de renard"
+print(titre.capitalize())
+print(titre.title())
+
+print(nom + ' aime le roman "' + titre + '".')
+print("j'aime Python")
+print('j\'aime Python')
---
+++ Chaines de caractères/chaine02.py
@@ -0,0 +1,9 @@
+nom = "andré".capitalize()
+age = 20
+
+print(nom + " est agé de " + str(age) + " ans.")
+
+print("{} est agé de {} ans.".format(nom, age))
+
+delai =8
+print(f"{nom} est agé de {age} ans et dans {delai} ans il aura {age+delai} ans.")
